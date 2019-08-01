a53fc00134fd9d2bd0bc7788aeb387acb4e19047
Jessie Cheng <xjessiechengx@gmail.com>
2019-08-01 16:10:01 +0800

    JPY

--- engine/currencySearch.py
+++ engine/currencySearch.py
@@ -3,5 +3,5 @@ import twder
 def currencySearch(search):
 	dollerTuple = twder.now_all()[search]
 	#reply = '{}\n{}即期賣出價:{}'.format(dollerTuple[0],search,dollerTuple[4])
-	reply = '{}\n{}現金買入價:{}\n現金賣出價:{}\n即期買入價:{}\n即期賣出價:{}'.format(dollerTuple[0],search,dollerTuple[1],dollerTuple[2],dollerTuple[3],dollerTuple[4])
+	reply = '{}\n{}\n現金買入價:{}\n現金賣出價:{}\n即期買入價:{}\n即期賣出價:{}'.format(dollerTuple[0],search,dollerTuple[1],dollerTuple[2],dollerTuple[3],dollerTuple[4])
 	return reply
